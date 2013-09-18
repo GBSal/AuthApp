e1dafca3ac89aee74455b2ab761b1a1908b4d27a
salsam <salman_it24@hotmail.com>
2013-09-18 20:08:30 +0100

    Update LoginController.js
    Updated the URl

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -4,7 +4,9 @@
     $scope.doLogin = function () {
       
         var data = { Username: $scope.Username, Password: $scope.Password, RememberMe: !!$scope.RememberMe };
-        var url = '/api/Values';
+        
+         var url = 'http://192.168.1.2:62404/api/Values/Get';
+        //var url = '/api/Values';
     
         $http({ method: "POST", url: url, data: data })
 
